1fb1d56f36320429fc995918f752522238623aba
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2023-02-03 23:14:24 +0200

    init: initialize new (Vue3 + Vite + Vuetify3) project

---
+++ src/modules/public/components/home/Banner.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="banner">
+    <h1>home banner</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Banner",
+};
+</script>
---
+++ src/modules/public/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-card>
+    <v-layout>
+      <v-app-bar elevation="0" color="transparent">
+        <v-toolbar-title class="text-h5">Home</v-toolbar-title>
+      </v-app-bar>
+
+      <v-main style="min-height: 150vh">
+        <banner />
+      </v-main>
+    </v-layout>
+  </v-card>
+</template>
+
+<script>
+import Banner from "../components/home/Banner.vue";
+
+export default {
+  name: "Home",
+
+  components: {
+    Banner,
+  },
+};
+</script>
